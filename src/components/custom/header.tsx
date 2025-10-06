e0cb328460308b4b661ce505b6e8b619d33c98fb
Jaya Vignesh <vickysanthosh2005@gmail.com>
2025-10-06 09:51:35 +0000

    changes

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -32,7 +32,7 @@ export function Header() {
   return (
     <header
       className={cn(
-        "fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out bg-background/90 backdrop-blur-lg",
+        "fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out",
         isScrolled && "bg-background/90"
       )}
     >
