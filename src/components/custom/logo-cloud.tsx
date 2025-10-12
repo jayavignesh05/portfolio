f484415e1b6f0bb89b9b637c8fbc46d7a3af0f62
Jaya Vignesh <vickysanthosh2005@gmail.com>
2025-10-12 06:21:05 +0000

    import { SiTypescript } from "react-icons/si"; add this

--- src/components/custom/logo-cloud.tsx
+++ src/components/custom/logo-cloud.tsx
@@ -8,12 +8,13 @@ import {
 import { ImHtmlFive } from "react-icons/im";
 import { FaCss3Alt, FaJsSquare } from "react-icons/fa";
 import { IoLogoNodejs } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
 
 const logos = [
   { icon: <Figma className="h-12 w-12" />, name: "Figma" },
   { icon: <Github className="h-12 w-12" />, name: "Github" },
   { icon: <ReactIcon className="h-12 w-12" />, name: "React" },
-  { icon: <TypeScriptIcon className="h-12 w-12" />, name: "TypeScript" },
+  { icon: <SiTypescript className="h-12 w-12" />, name: "TypeScript" },
   { icon: <FaJsSquare className="h-12 w-12" />, name: "JavaScript" },
   { icon: <ImHtmlFive className="h-12 w-12" />, name: "HTML5" },
   { icon: <FaCss3Alt className="h-12 w-12" />, name: "CSS3" },
@@ -62,19 +63,6 @@ function ReactIcon(props: React.SVGProps<SVGSVGElement>) {
         <ellipse rx="11" ry="4.2" transform="rotate(60)" />
         <ellipse rx="11" ry="4.2" transform="rotate(120)" />
       </g>
-    </svg>
-  );
-}
-
-function TypeScriptIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-    >
-      <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.361 10.264h4.256v2.016h-4.256v-2.016zm0 4.232h4.256v2.016h-4.256v-2.016zM18.42 5.928H5.58l.736 8.352h11.368l.448-5.024h-2.24l-.224 2.512H8.32l-.24-2.736h10.56l.24-2.736.24-2.736.256-2.624H5.58l.736 8.352.24-2.736z" />
     </svg>
   );
 }
