2b4e29b80ec35e328c297cc53110dbc4f1ee766b
Jaya Vignesh <vickysanthosh2005@gmail.com>
2025-11-19 10:10:31 +0000

    <AboutSection /> ethula animtion add panu import AnimatedContent from '.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { ContactSection } from "@/components/custom/contact-section";
 import { useEffect, useState } from "react";
 import { TechStack } from "@/components/custom/tech-stack";
 import { ZoomOnScrollImage } from "@/components/custom/zoom-on-scroll-image";
+import AnimatedContent from "@/components/AnimatedContent";
 
 export default function Home() {
 
@@ -17,9 +18,22 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <ZoomOnScrollImage src="/assets/hands.avif" alt="Hands typing on a laptop" />
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        duration={1.2}
+        ease="bounce.out"
+        initialOpacity={0.2}
+        animateOpacity
+        scale={1.1}
+        threshold={0.2}
+        delay={0.3}
+      >
+        <AboutSection />
+      </AnimatedContent>
       <ProjectsSection />
       <TechStack />
-      <AboutSection />
       <div className="bg-background">
         <ContactSection />
       </div>
